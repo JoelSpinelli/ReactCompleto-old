caf5255e9fc1761b9bad574aaf9fc5e91a115afd
JoelSpinelli <joel.spinelli@hotmail.com>
2020-08-13 21:40:50 -0300

    useState 2

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import ButtonModal from './ButtonModal';
-import Modal from './Modal';
+import React, {useState} from 'react'
 
 const App = () => {
-  const [modal, setModal] = React.useState(false)
+  const [contar, setContar] = React.useState(1);
+  const [items, setItems] = React.useState(['Item 1']);
 
+  function handleClick() {
+    setContar((contar) => contar + 1);
+    setItems((items) => [...items, 'Item ' + (contar + 1)])
+  }
 
   return (
     <div>
-      <div>{modal ? 'modal aberto': 'modal Fechado'}</div>
-      <Modal modal={modal} setModal={setModal} />
-      <ButtonModal setModal={setModal} />
+      {items.map(item => <li key={item}>{item}</li>)}
+      <button onClick={handleClick}>{contar}</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
