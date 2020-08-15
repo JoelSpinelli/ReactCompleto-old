99fb5ee145a47a967f6e583c521cb2b565611184
JoelSpinelli <joel.spinelli@hotmail.com>
2020-08-15 20:04:53 -0300

    useContext

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,43 +1,12 @@
 import React from 'react'
-
-const set1 = new Set();
-const set2 = new Set();
-
-const Produto = () => {
-  const func1 = () => {
-    console.log('Teste')
-  }
-
-  const func2 = React.useCallback(() => {
-    console.log('Teste2');
-  }, []);
-
-  set1.add(func1)
-  set2.add(func2)
-
-  console.log('Set 1', set1)
-  console.log('Set 2', set2)
-
-  return (
-    <div>
-      <p onClick={func1}>Produto 1</p>
-      <p onClick={func2}>Produto 2</p>
-    </div>
-  )
-}
+import Produto from './Produto';
+import { GlobalStorage } from './GlobalContext';
 
 const App = () => {
-  const [contar, setContar] = React.useState(0);
-
-  const handleClick = React.useCallback(() => {
-    setContar((contar) => contar + 1)
-  }, []);
-
   return (
-    <>
-    <Produto />
-      <button onClick={() => setContar(contar + 1)}>{contar}</button>
-    </>
+    <GlobalStorage>
+      <Produto />
+    </GlobalStorage>
   );
 }
 
