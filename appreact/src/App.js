4329489297d310a7af3e23f2d4d46d256e37ef2b
JoelSpinelli <joel.spinelli@hotmail.com>
2020-08-23 17:25:40 -0300

    exercicio checkbox

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,40 +1,38 @@
 import React from 'react'
 
 const App = () => {
+  const coresArray = ['azul', 'roxo', 'laranja', 'verde', 'vermelho', 'cinza'];
+
   const [cores, setCores] = React.useState([]);
 
-  function handleChange({target}) {
-    if(target.checked){
+  function handleChange({ target }) {
+    if (target.checked) {
       setCores([...cores, target.value]);
-      return;
+    } else {
+      setCores(cores.filter((cor) => cor !== target.value));
     }
-
-    setCores(cores.filter(cor => cor!== target.value));
   }
 
-  return(
+  function handleChecked(cor) {
+    return cores.includes(cor);
+  }
+
+  return (
     <form>
-      <label>
-        <input
-          type="checkbox"
-          value="azul"
-          checked={cores.includes('azul')}
-          onChange={handleChange}
-        />
-        Azul
-      </label>
-      <label>
-        <input
-          type="checkbox"
-          value="vermelho"
-          checked={cores.includes('vermelho')}
-          onChange={handleChange}
-        />
-        Vermelho
-      </label>
+      {coresArray.map((cor, index) =>(
+        <label key= { index } style={{textTransform: 'capitalize'}}>
+          <input
+            type="checkbox"
+            value={cor}
+            checked={handleChecked(cor)}
+            onChange={handleChange}
+          />
+          {cor}
+        </label>
+      ))}
     </form>
-  )
-}
+  );
+};
 
 export default App
 
