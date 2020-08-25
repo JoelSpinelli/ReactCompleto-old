7bcec8323fe2b94722c84604f75b5127b147d508
JoelSpinelli <joel.spinelli@hotmail.com>
2020-08-24 21:43:01 -0300

    Componente Checkbox

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import Input from './Form/Input'
 import Select from './Form/Select';
+import Radio from './Form/Radio';
 
 const App = () => {
   const [nome, setNome] = React.useState('');
   const [email, setEmail] = React.useState('');
   const [produto, setProduto] = React.useState('')
+  const [cor, setCor] = React.useState('');
 
   return (
     <form>
+      <Radio options={['Azul', 'Vermelho' ]} value={cor} setValue={setCor}/>
       <Select options={['smartphone', 'notebook']} value={produto} setValue={setProduto} />
       <Input id="nome" label="Nome" value={nome} setValue={setNome} />
       <Input id="email" label="Email" value={email} setValue={setEmail}/>
