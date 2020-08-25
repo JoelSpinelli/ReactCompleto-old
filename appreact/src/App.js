a9768ecb714ccf3273a12507cf2d076d0e50cd9c
JoelSpinelli <joel.spinelli@hotmail.com>
2020-08-24 22:03:26 -0300

    Componente checkbox

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -2,22 +2,32 @@ import React from 'react'
 import Input from './Form/Input'
 import Select from './Form/Select';
 import Radio from './Form/Radio';
+import Checkbox from './Form/Checkbox';
 
 const App = () => {
   const [nome, setNome] = React.useState('');
   const [email, setEmail] = React.useState('');
   const [produto, setProduto] = React.useState('')
   const [cor, setCor] = React.useState('');
+  const [linguagens, setLinguages] = React.useState([]);
 
   return (
     <form>
-      <Radio options={['Azul', 'Vermelho' ]} value={cor} setValue={setCor}/>
-      <Select options={['smartphone', 'notebook']} value={produto} setValue={setProduto} />
-      <Input id="nome" label="Nome" value={nome} setValue={setNome} />
-      <Input id="email" label="Email" value={email} setValue={setEmail}/>
+      <Checkbox
+        options={['JavaScript', 'PHP', 'Ruby']}
+        value={linguagens}
+        setValue={setLinguages}
+      />
+      <Radio options={['Azul', 'Vermelho']} value={cor} setValue={setCor} />
+      <Select
+        options={['smartphone', 'notebook']}
+        value={produto}
+        setValue={setProduto}
+      />
+      <Input id='nome' label='Nome' value={nome} setValue={setNome} />
+      <Input id='email' label='Email' value={email} setValue={setEmail} />
     </form>
   );
 };
 
 export default App
-
