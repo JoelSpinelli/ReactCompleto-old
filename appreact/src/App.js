8fedd384918bb1ca8e10f8216f5be82addeb9737
JoelSpinelli <joel.spinelli@hotmail.com>
2020-09-06 11:49:19 -0300

    useForm

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,54 +1,9 @@
 import React from 'react'
-import Input from './Form/Input'
 
 const App = () => {
-  const [cep, setCep] = React.useState('');
-  const [error, setError] = React.useState(null);
-
-  function validateCep(value) {
-    if(value.length === 0) {
-      setError('Preencha um valor');
-      return false;
-    } else if(!/^\d{5}-?\d{3}$/.test(value)) {
-      setError('Preencha um CEP válido');
-      return false;
-    } else {
-      setError(null);
-      return true;
-    }
-  }
-
-  function handleBlur ({target}) {
-    validateCep(target.value);
-  }
-
-  function handleChange({target}) {
-    if(error) validateCep(target.value);
-    setCep(target.value);
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    if(validateCep(cep)) {
-      console.log('enviou');
-    } else{
-      console.log('não enviar');
-    }
-  }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Input
-        type='text'
-        label='CEP'
-        id='cep'
-        value={cep}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        placeholder='00000-000'
-      />
-      {error && <p>{error}</p>}
-      <button>Enviar</button>
+    <form>
     </form>
   );
 };
